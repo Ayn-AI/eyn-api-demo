dffd8bfcd40390896a7382c9c81c77fb298e6a9f
Robin Ankele <robin@eyn.vision>
2020-07-06 16:33:15 +0100

    add demo for immunity verify

---
+++ immunity/demo_immunity_verify.py
@@ -0,0 +1,17 @@
+import requests
+import os
+import json
+import base64
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "certificate.jpeg"), "rb") as file:
+    blob = base64.encodebytes(file.read())
+    file.close()
+    blob = blob.decode("ascii")
+    certificate_base64_encoded = blob.replace("\n", "")
+
+data = {
+    'api_secret': '676bc3ca-e2b9-4161-85a4-1dc8592916a5',
+    'certificate': certificate_base64_encoded,
+}
+response = requests.post('https://api.eyn.ninja/api/v1/prod/immunity_verify', json=data)
+print(response.text)
