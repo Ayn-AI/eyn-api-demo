2e3e787de5eab22d96a3104f39550bcc6ec94746
Robin Ankele <robin@eyn.vision>
2020-08-12 14:13:34 +0100

    add docu

--- eu-settlement/demo_eu_settlement.py
+++ eu-settlement/demo_eu_settlement.py
@@ -1,8 +1,11 @@
 import requests
 
 data = {
+    # update with the api secret obtained from EYN (contact@eyn.vision)
     'api_secret': '9b7539bc-5f62-420d-ba4d-2241ade8f4b6',
+    # update with a share code obtained from https://view-immigration-status.service.gov.uk
     'share_code': 'JP7LFW2FP',
+    # update with the candidate's birth date
     'date_of_birth': '04/12/1988',
     'company_name': 'EYN',
 }
